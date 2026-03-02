97a192ca945b6c2e66434651933099115c53b9c1
nlothian500 <lothiannathan@gmail.com>
2026-03-02 02:18:09 +0000

    tictactoe March 1st 2026

---
+++ .github/TicTactoe.py
@@ -0,0 +1,24 @@
+board = [[],[],[]],[[],[],[]],[[],[],[]]
+for i in range(3):
+    print(board[i])
+
+while True:
+    row = int(input("Enter your move (row):"))
+
+    col = int(input("Enter your move (column):"))
+    if board[row][col] == "x" 
+    board[row][col] = "x"
+    for i in range(3):
+        print(board[i])
+
+    row = int(input("Enter your move (row):"))
+
+    col = int(input("Enter your move (column):"))
+
+    board[row][col] = "o"
+    for i in range(3):
+        print(board[i])
+
+
+
+
