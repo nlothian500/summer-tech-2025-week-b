8c3747e0b6235f87df909725782c9cf15361d7b7
nlothian500 <lothiannathan@gmail.com>
2026-07-27 15:15:44 +0000

    new changes good

--- ocean explorer.py
+++ ocean explorer.py
@@ -1,17 +1,19 @@
 from PIL import Image
 from transformers import pipeline
 
-x = Image.open("fish.webp")
+x = Image.open("Barracuda_laban.jpg")
 x.show()
 print(x)
-j  = pipeline("zero shot-classificarion")
-fish = []
-fish = ["Red Snapper", "mackerel", "tuna"
-"Mahi-Mahi", "Parrotfish", "Barracuda"
+classifier  = pipeline("zero-shot-image-classification")
+
+fish = ["Red Snapper", "mackerel", "tuna",
+"Mahi-Mahi", "Parrotfish", "Barracuda",
 "jack"]
 
-fish = ["Red Snapper", "mackerel", "tuna","Mahi-Mahi", "Parrotfish", "Barracuda","jack"]
-inputs = classifier(images=fish.webp, text=fish,)
+inputs = classifier(x, candidate_labels=fish)
+
+print(inputs)
+
                     
 
 
