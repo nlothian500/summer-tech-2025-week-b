9af48ff2f5d83ac5c8a2fd724cc9503c7eff1ebe
nlothian500 <lothiannathan@gmail.com>
2026-07-29 17:31:22 +0000

    yes

--- Tic tac toe.py
+++ Tic tac toe.py
@@ -1,24 +1,28 @@
 board = [["-","-","-"], ["-","-","-"],["-","-","-"]]
-#print(board)
-#board[1][1] ="x"
-for b in range(3):
-    print( )
-    for j in range(3):
-        print(board[b][j],end="")
 
-player1 = "x"
-player2 = "o"
-w = int(input("enter your row number"))
-f = int(input("enter your column number"))
-board[0][0] = "x"
-print(board)
+player = "x"
+runningame = True
+while runningame == True:
+    for b in range(3):
+        for j in range(3):
+            print(board[b][j],end="")
+        print( )
 
+    
+    w = int(input("enter your row number"))
+    f = int(input("enter your column number"))
+    board[w][f] = player
 
+    if player == [0][0] and player == [0][1] and player == [0][2]:
+        print(player+"won")
+        board == [0][0] and [0][1] and [0][2]
+    if player == [1][0] and player == [1][1] and player == [1][2]:
+        print(player+"won")
+    if player == [2][0] and player == [2][1] and player == [2][2]:
+        print(player+"won")
 
-
-
-
-
-
-
+    if player == "x":
+        player = "o"
+    elif player == "o":
+        player = "x"
 
