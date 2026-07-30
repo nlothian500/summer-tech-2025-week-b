49d6008008189ddc3209a820e422dc70c9b8e711
nlothian500 <lothiannathan@gmail.com>
2026-07-30 20:44:03 +0000

    fds

---
+++ calculator.py
@@ -0,0 +1,28 @@
+a = int(input("Please enter your first number"))
+b = int(input("Please enter your second number"))
+print("please enter in your operation")
+op = input()
+
+if(op == "+"):
+    print(a+b)
+elif(op == "-"):
+    print(a-b)
+elif(op == "*"):
+    print(a*b)
+elif(op == "/"):
+    print(a/b)
+
+else:
+    print("you didnt pick one of the options")
+
+
+
+
+
+
+
+
+
+
+
+
