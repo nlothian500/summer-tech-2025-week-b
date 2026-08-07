e2012665e3cef49972a46316fe1ac0fa1cc04f17
nlothian500 <lothiannathan@gmail.com>
2026-08-07 00:28:21 +0000

    7777

--- Tic tac toe.py
+++ Tic tac toe.py
@@ -13,25 +13,23 @@ while runningame == True:
     f = int(input("enter your column number"))
     board[w][f] = player
 
-    if board[0][0] == player == player and [0][1] == player and [0][2] == player:
-        print(player+"won")
+    if board[0][0] == player and board[0][1] == player and board[0][2] == player:
+        print(player + "won")
+        runningame = False
      
-    if [1][0] == player and  [1][1] == player and  == [1][2]:
-        print(player+"won")
-       
-    if player == [2][0] and player == [2][1] and player == [2][2]:
-        print(player+"won")
+    if board[1][0] == player and board[1][1] == player and board[1][2] == player:
+        print(player + "won")
+        runningame = False
 
+    if board[2][0] == player and board[2][1] == player and board[2][2] == player:
+        print(player + "won")
+        runningame = False
+    
     if player == "x":
         player = "o"
     elif player == "o":
         player = "x"
     
-    runningame = False
-
-
-
-
 
 
 player == [0][0]
