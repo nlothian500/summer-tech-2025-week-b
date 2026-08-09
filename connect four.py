a4b245f8c86c61b40633ec087c902aaaab8d906d
nlothian500 <lothiannathan@gmail.com>
2026-08-09 03:30:42 +0000

    ww

---
+++ connect four.py
@@ -0,0 +1,14 @@
+board = [["-","-","-"], ["-","-","-"], ["-","-","-"], ["-","-","-"]  ]
+for a in range(6):
+    for c in range(6):
+        print(board[a][c],end="")
+    print( )
+
+
+
+
+
+
+
+
+
